a38bf5a4e501ad6ff334d825fe2bcaa2b7ceb45c
fchiave <fchiave@ad.unc.edu>
2022-03-29 13:27:58 -0400

    printing help message, need to do only on --help

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-// Import coin methods
-import {coinFlip, coinFlips, countFlips, flipACoin} from './modules/coin.mjs';
-// Require Express.js
-import express from 'express'
-const app = express()
-
 // Import minimist to help get command line args
 import minimist from 'minimist';
 const args = minimist(process.argv.slice(2))
+console.log(args)
+if (true) {
+    console.log(`server.js [options]
+
+    --port	Set the port number for the server to listen on. Must be an integer
+                between 1 and 65535.
+  
+    --debug	If set to \`true\`, creates endlpoints /app/log/access/ which returns
+                a JSON access log from the database and /app/error which throws 
+                an error with the message "Error test successful." Defaults to 
+                \`false\`.
+  
+    --log		If set to false, no log files are written. Defaults to true.
+                Logs are always written to database.
+  
+    --help	Return this message and exit.`)
+    process.exit(0);
+}
+
+// Import coin methods
+// import {coinFlip, coinFlips, countFlips, flipACoin} from './modules/coin.mjs';
+// Require Express.js
+import express from 'express'
+const app = express()
 
 // Set port to arg or default to 5000
 const port = args.port || process.env.PORT || 5000
