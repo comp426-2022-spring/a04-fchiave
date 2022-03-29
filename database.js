a72627676863d22c4bc106aa6ee6e9670f813fdd
fchiave <fchiave@ad.unc.edu>
2022-03-29 14:42:24 -0400

    renamed table

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ import bettersqlite3 from 'better-sqlite3'
 const db = new bettersqlite3('log.db');
 // Is the database initialized or do we need to initialize it?
 const stmt = db.prepare(`
-    SELECT name FROM sqlite_master WHERE type='table' and name='logdata';`
+    SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
     );
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();
@@ -15,7 +15,7 @@ if (row === undefined) {
     console.log('Your database appears to be empty. I will initialize it now.');
 // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
-        CREATE TABLE logdata ( remoteaddr INTEGER PRIMARY KEY, remoteuser TEXT, time TEXT, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, secure TEXT, status TEXT, referer TEXT, useragent TEXT );
+        CREATE TABLE accesslog ( remoteaddr INTEGER PRIMARY KEY, remoteuser TEXT, time TEXT, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, secure TEXT, status TEXT, referer TEXT, useragent TEXT );
     `;
     // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
